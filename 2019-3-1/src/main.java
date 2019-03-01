76f8504825bab60c95f13a37c4db564bc0c0934c
AilPaiPro <635579172@qq.com>
2019-03-01 15:57:09 +0800

    2019.3.1 JAVA learn
    15.54 JAVA FIRST LEARN

---
+++ 2019-3-1/src/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+  public static void main(String[] args) {
+    System.out.println("Hello Word!");
+    if (args.length != 0)
+      System.out.println("Hello" + args[0] + "!");
+  }
+
+}
